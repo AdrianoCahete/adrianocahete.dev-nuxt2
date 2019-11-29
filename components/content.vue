783f40e5a6bd65dee4f2c1266cfcd1cfac1665d2
Adriano Cahete <nano.rj@gmail.com>
2019-11-28 23:17:32 -0300

    Adding more content and creating styles

---
+++ components/content.vue
@@ -0,0 +1,79 @@
+<template>
+  <div class="content">
+    <pageHome />
+    <div id="work" class="section">
+      <h1 class="title">Work Experience</h1>
+      <p class="description">I have over 9 years of experience in UI/UX design, UI Development and know the basics of a few Front End and Backend languages</p>
+
+      <div class="card">
+        <h2 class="cardTitle">Company 1</h2>
+        <h3 class="cardSubTitle">Work Position</h3>
+        <ul class="cardItems">
+          <li>a</li>
+          <li>b</li>
+          <li>c</li>
+        </ul>
+      </div>
+
+      <div class="card">
+        <h2 class="cardTitle">Company 2</h2>
+        <h3 class="cardSubTitle">Work Position</h3>
+        <ul class="cardItems">
+          <li>a</li>
+          <li>b</li>
+          <li>c</li>
+        </ul>
+      </div>
+
+    </div>
+    <div id="projects" class="section">
+      Projects Projects Projects Projects Projects Projects Projects Projects Projects Projects Projects Projects Projects Projects Projects Projects
+      Projects Projects Projects Projects Projects Projects Projects Projects Projects Projects Projects Projects Projects Projects Projects Projects
+      Projects Projects Projects Projects Projects Projects Projects Projects Projects Projects Projects Projects Projects Projects Projects Projects
+      Projects Projects Projects Projects Projects Projects Projects Projects Projects Projects Projects Projects Projects Projects Projects Projects
+      Projects Projects Projects Projects Projects Projects Projects Projects Projects Projects Projects Projects Projects Projects Projects Projects
+      Projects Projects Projects Projects Projects Projects Projects Projects Projects Projects Projects Projects Projects Projects Projects Projects
+      Projects Projects Projects Projects Projects Projects Projects Projects Projects Projects Projects Projects Projects Projects Projects Projects
+      Projects Projects Projects Projects Projects Projects Projects Projects Projects Projects Projects Projects Projects Projects Projects Projects
+    </div>
+    <div id="writings" class="section">
+      writings writings writings writings writings writings writings writings writings writings writings writings writings writings writings writings
+      writings writings writings writings writings writings writings writings writings writings writings writings writings writings writings writings
+      writings writings writings writings writings writings writings writings writings writings writings writings writings writings writings writings
+      writings writings writings writings writings writings writings writings writings writings writings writings writings writings writings writings
+      writings writings writings writings writings writings writings writings writings writings writings writings writings writings writings writings
+      writings writings writings writings writings writings writings writings writings writings writings writings writings writings writings writings
+      writings writings writings writings writings writings writings writings writings writings writings writings writings writings writings writings
+      writings writings writings writings writings writings writings writings writings writings writings writings writings writings writings writings
+      writings writings writings writings writings writings writings writings writings writings writings writings writings writings writings writings
+      writings writings writings writings writings writings writings writings writings writings writings writings writings writings writings writings
+    </div>
+  </div>
+</template>
+
+<script>
+import pageHome from '~/components/pageComponents/home.vue'
+
+export default {
+  // layout: 'blog'
+  components: {
+    pageHome
+  },
+}
+</script>
+
+<style lang="stylus" scoped>
+@import '~assets/styles/components/section.styl';
+
+.content {
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+  align-items: center;
+  text-align: center;
+  margin: 0 auto;
+  width: 80vw;
+  scroll-snap-type: y mandatory;
+  scroll-snap-align: start;
+}
+</style>
---
+++ components/pageComponents/home.vue
@@ -0,0 +1,44 @@
+<template>
+    <div id="home" class="section isCentered">
+      <logo />
+      <h1 class="title">
+        Adriano Cahete
+      </h1>
+      <p>
+        I am a <strong>User Interface Developer</strong>, <strong>User Interface</strong> & <strong>User Experience Designer</strong>
+      </p>
+      <p>
+        currently working at <a href="#">SAI Global</a>
+      </p>
+    </div>
+</template>
+
+<script>
+import Logo from '~/components/Logo.vue'
+
+export default {
+  // layout: 'blog'
+  components: {
+    Logo
+  },
+
+  head () {
+    return {
+      title: 'Home',
+      meta: [
+        // hid is used as unique identifier. Do not use `vmid` for it as it will not work
+        //{ hid: 'description', name: 'description', content: 'Adriano\'s Cahete Portfolio' }
+      ]
+    }
+  }
+}
+</script>
+
+<style lang="stylus" scoped>
+.title {
+  display: block;
+  font-weight: 300;
+  font-size: 100px;
+  letter-spacing: 1px;
+}
+</style>
