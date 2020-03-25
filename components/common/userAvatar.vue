a3bd9ca8652668546cb4ec90d878a1ca759dfab9
Adriano Cahete <nano.rj@gmail.com>
2020-03-25 10:48:47 -0300

    Move text color to custom css vars

--- components/common/userAvatar.vue
+++ components/common/userAvatar.vue
@@ -18,12 +18,11 @@ export default {
 
 <style lang="stylus" scoped>
 .avatar {
-
     > img {
       width: 8rem;
       height: @width;
       min-height: 80px;
-      background-color: #fff;
+      background-color: var(--textColor, #ffffff);
       border-radius: 50%;
       border: 2px solid #ccc; // Move to vars
       // TODO: Create animation on render
