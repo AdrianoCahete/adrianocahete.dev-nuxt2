0486d9260a56840f8ad9aa87a860b9863daabdc9
Adriano Cahete <nano.rj@gmail.com>
2020-03-25 10:47:12 -0300

    Add SVG as a Library component

---
+++ components/common/svgLinks.vue
@@ -0,0 +1,31 @@
+<template>
+  <!-- <div class="avatar">
+    <img :src="'https://github.com/'+ userName + '.png'">
+  </div> -->
+  <a :href="targetURL" target="_blank" rel="nofollow,noopener,external">
+    <svg class="">
+      <use :xlink:href="'#'+ icon"></use>
+    </svg>
+  </a>
+</template>
+
+<script>
+export default {
+  props: {
+    'targetURL': {
+      type: String,
+      required: true,
+      default: '#'
+    },
+    'icon': {
+      type: String,
+      required: true,
+      default: ''
+    }
+  }
+}
+</script>
+
+<style lang="stylus" scoped>
+
+</style>
