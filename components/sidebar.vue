30ef02e46681d0dff487f054f30a817e9352c2f8
Adriano Cahete <nano.rj@gmail.com>
2020-03-25 20:11:42 -0300

    Add resume for download on pdf

--- components/sidebar.vue
+++ components/sidebar.vue
@@ -20,7 +20,12 @@
       <svgLinks url="https://github.com/AdrianoCahete" icon="github" title="Github" />
       <svgLinks url="https://linkedin.com/in/AdrianoCahete/" icon="linkedin" title="LinkedIn" />
       <svgLinks url="https://codepen.io/AdrianoCahete/" icon="codepen" title="CodePen" />
+      <svgLinks url="./Resume-Adriano_Cahete__en_US_2020.03.pdf" icon="pdf" title="Resume" /> <!-- TODO: Update when update static file -->
       <!-- <a href="#" target="#">?</a> -->
+    </section>
+    <!-- TODO: Fix flex disposition of about section -->
+    <section class="about" style="display: none">
+      <svgLinks url="https://github.com/AdrianoCahete/adrianocahete.dev" icon="github" title="This site is OpenSourced @ Github" />
     </section>
   </section>
 </template>
