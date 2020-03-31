9175eef272d685d66c76b61191477c4d588c792f
Adriano Cahete <nano.rj@gmail.com>
2020-03-31 14:32:39 -0300

    Fix remote work info on SAI

--- components/pageComponents/work.vue
+++ components/pageComponents/work.vue
@@ -16,7 +16,7 @@
         SAI Global (US/Netherlands)
       </h3>
       <h4 class="cardInfo">
-        Jan 2017 - Present | Partially Remote
+        Jan 2017 - Present | Remote
       </h4>
       <ul class="cardItems">
         <li>
