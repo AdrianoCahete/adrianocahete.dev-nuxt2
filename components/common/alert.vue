89b02ba423e487b9ebae99f74997ac0af426419c
Adriano Cahete <me@adrianocahete.dev>
2020-04-15 01:35:32 -0300

    Add alert component + Add behance links to interface page

---
+++ components/common/alert.vue
@@ -0,0 +1,55 @@
+<template>
+  <div :class="'alert ' + 'alert-' + type">
+    <div class="alertIcon">
+      <!-- TODO: Add v-if to icon based on type -->
+      {{ icon }}
+    </div>
+    <div>
+      {{ message }}
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  // components: {
+  // },
+
+  props: {
+    type: {
+      type: String,
+      required: true,
+      default: ''
+    },
+    icon: {
+      type: String,
+      required: false,
+      default: ''
+    },
+    message: {
+      type: String,
+      required: true,
+      default: ''
+    }
+  }
+}
+</script>
+
+<style lang="stylus" scoped>
+.alert {
+  padding: 1vh 1vw;
+  background: var(--scrollbarColor); // TODO: Move to var
+  margin: 1vw 0;
+
+  &.alert-info {
+    background-color: var(--alertInfo);
+    color: #fff;
+  }
+
+  .alertIcon {
+    &:empty {
+      display: none;
+    }
+  }
+}
+</style>
