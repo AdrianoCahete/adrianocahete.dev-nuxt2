ff8d365649b0878febc605c19569b751f2eed9fe
Adriano Cahete <me@adrianocahete.dev>
2020-07-30 01:40:22 -0300

    Fixing if mobile on navbar error

--- components/common/navbar.vue
+++ components/common/navbar.vue
@@ -11,8 +11,9 @@
       <!-- https://br.vuejs.org/v2/guide/syntax.html#Parametros -->
       <!-- TODO: Move Home to Icon instead of text and remove isHidden -->
       <nuxt-link to="/" exact>
-        <span v-if="!$device.isMobile">Home</span>
-        <span v-if="$device.isMobile"><Icon icon="home" title="Home" /></span>
+        <span>Home</span>
+        <!-- <span v-if="!$device.isMobile">Home</span> -->
+        <!-- <span v-if="$device.isMobile"><Icon icon="home" title="Home" /></span> -->
       </nuxt-link>
       <nuxt-link to="/about">
         About
