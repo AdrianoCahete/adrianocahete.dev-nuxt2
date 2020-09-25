6bb8cc79c678fe5a2632172b1d730fd5ba1a5dba
Adriano Cahete <me@adrianocahete.dev>
2020-09-25 12:53:32 -0300

    Removing unused code

--- external/history/history.js
+++
@@ -1,9 +0,0 @@
-const { webkit } = require('playwright');
-
-(async () => {
-  const browser = await webkit.launch()
-  const page = await browser.newPage()
-  await page.goto('http://localhost:3002/')
-  await page.screenshot({ path: 'images/example.png' })
-  await browser.close()
-})()
