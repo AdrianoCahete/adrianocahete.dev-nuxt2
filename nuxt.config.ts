34328316ffe7df150309369292c4306bec82fbd3
Adriano Cahete <me@adrianocahete.dev>
2024-02-16 22:16:39 -0300

    feat(upgrade): update nuxt3 configs

---
+++ nuxt.config.ts
@@ -0,0 +1,103 @@
+// https://nuxt.com/docs/api/configuration/nuxt-config
+export default defineNuxtConfig({
+  devtools: { enabled: true },
+  vue: {
+    propsDestructure: true,
+  },
+
+  // WebApp Config
+  app: {
+    head: {
+      meta: [
+        {
+          name: "viewport",
+          content: "width=device-width, initial-scale=1, user-scalable=1",
+        },
+        {
+          name: "keywords",
+          content:
+            "FrontEnd Developer, FrontEnd, Front-End, Front End, Design Technologist, designops, designerops, design ops, designer ops, product design, product designer, designer de produto, UI, UX, UI/UX, UI developer, Desenvolvedor de Interface, Designer, Cahete, Adriano Cahete, Design Enginner",
+        },
+        {
+          hid: "description",
+          name: "description",
+          content: "", // process.env.npm_package_description
+        },
+        {
+          name: "author",
+          content: "Adriano Cahete", // process.env.npm_package_author
+        },
+        {
+          name: "theme-color",
+          content: "#175e80", // process.env.npm_package_project_theme_color
+        },
+
+        { property: "og:type", content: "website" },
+        // { property: "og:title", content: process.env.npm_package_description },
+        // { property: "og:url", content: process.env.npm_package_url },
+        {
+          property: "og:site_name",
+          // content: process.env.npm_package_description,
+        },
+        {
+          property: "og:description",
+          // content: process.env.npm_package_description,
+        },
+        {
+          property: "og:image",
+          itemprop: "image",
+          name: "image",
+          // content: process.env.npm_package_url + "/cover.png",
+        },
+        {
+          property: "og:image:secure_url",
+          itemprop: "image",
+          name: "image",
+          // content: process.env.npm_package_url + "/cover.png",
+        },
+        { name: "twitter:card", content: "summary_large_image" },
+      ],
+      noscript: [{ children: "JavaScript is required" }],
+    },
+
+    // Legacy
+    // manifest: {
+    //   name:
+    //     process.env.npm_package_author_title ||
+    //     "Adriano Cahete | Design Technologist & DesignOps",
+    //   short_name:
+    //     process.env.npm_package_author_title ||
+    //     "Adriano Cahete | Design Technologist & DesignOps",
+    //   description:
+    //     process.env.npm_package_description || "Design Technologist & DesignOps",
+    //   theme_color: process.env.npm_package_project_theme_color || "#175e80",
+    //   display: "minimal- ui",
+    // },
+
+    // meta: {
+    //   mobileAppIOS: true,
+    //   appleStatusBarStyle: "black-translucent",
+    // },
+
+    // // Customize the progress-bar color
+    // // https://nuxtjs.org/api/configuration-loading
+    // // loading: '~/components/common/loading.vue',
+    // loading: { color: "#FFFFFF" },
+
+    // pageTransition: {
+    //   name: "page",
+    //   mode: "out-in",
+    // },
+
+    // layoutTransition: {
+    //   name: "layout",
+    //   mode: "out-in",
+    // },
+
+    // sitemap: {
+    //   hostname: "https://adrianocahete.dev",
+    //   gzip: true,
+    //   exclude: ["/debug", "/resume"],
+    // },
+  },
+});
